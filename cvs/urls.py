a78b668c444f66ed0463a7df0910ae6872d6cd52
HoCongThanh2205 <thanhhc2205@gmail.com>
2025-12-04 10:05:30 +0700

    chỉnh sửa url

--- cvs/urls.py
+++ cvs/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('upload/', views.upload_cv, name='upload_cv'),
+    path('jobs/', views.job_list, name='job_list'),
+    path('candidates/', views.candidate_list, name='candidate_list'),
     path('apply/<int:cv_id>/<int:job_id>/', views.apply_job, name='apply_job'),
 ]
